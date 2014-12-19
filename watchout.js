392b6236389ed2a6444dce687229ddaf6f0d162f
dsernst <github@dsernst.com>
2014-12-19 15:58:40 -0800

    Draw the enemies in an svg element

--- watchout.js
+++ watchout.js
@@ -1,2 +1,24 @@
+/*globals d3*/
 // start slingin' some d3 here.
 
+var width = 750,
+    height = 500;
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(32," + (height / 2) + ")");
+
+function update(data) {
+  var enemies = svg.selectAll("circle")
+      .data(data);
+
+
+  enemies.enter().append("circle")
+    .attr("class", "enemy")
+    .attr("r", 50);
+
+}
+
+update([1,2,3,4,5]);
