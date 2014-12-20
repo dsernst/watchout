1b599ff05a141ec1c222d3fb006abd5c891be1e2
dsernst <github@dsernst.com>
2014-12-19 20:00:05 -0800

    Detect when a enemy touches you.

--- watchout.js
+++ watchout.js
@@ -1,25 +1,23 @@
 /*globals d3*/
 // start slingin' some d3 here.
 
-var width = 750,
-    height = 500;
-
+// Game Configs
+var width = 750;
+var height = 500;
 var enemySize = 25;
-var xmin =  -height/2 + enemySize;
-var xmax =  height/2 - enemySize;
+
+var xmin =  0 + enemySize;
+var xmax =  height - enemySize;
 
 var svg = d3.select("body").append("svg")
     .attr("width", width)
-    .attr("height", height)
-  .append("g")
-    .attr("transform", "translate(32," + (height / 2) + ")");
+    .attr("height", height);
 
-var update = function () {
-  svg.selectAll("circle")
-    .filter(".enemy")
-    .transition()
-    .attr("cx", function(){return Math.random() * (width - 50);})
-    .attr("cy", function(){return Math.random() * (xmax - xmin) + xmin;});
+var updateEnemies = function () {
+  svg.selectAll("circle.enemy")
+  .transition()
+  .attr("cx", function(){return Math.random() * (width - 50);})
+  .attr("cy", function(){return Math.random() * (xmax - xmin) + xmin;});
 };
 
 var create = function (data) {
@@ -30,7 +28,7 @@ var create = function (data) {
     .attr("class", "enemy")
     .attr("r", enemySize);
 
-  update();
+  updateEnemies();
 };
 
 var drag = d3.behavior.drag()
@@ -39,15 +37,46 @@ var drag = d3.behavior.drag()
       d3.select(this).attr("cy", d3.event.y);
     });
 
-svg.append("circle")
+var player = svg.append("circle")
   .attr("class", "david")
   .attr("r", 15)
+  .attr("cx", width / 2)
+  .attr("cy", 40)
   .call(drag);
+
+var checkCollision = function (collidedCallback) {
+  svg.selectAll("circle.enemy")
+  .each(function() {
+    var enemy = d3.select(this);
+    var radiusSum =  parseFloat(enemy.attr("r")) + parseFloat(player.attr("r"));
+    var xDiff = parseFloat(enemy.attr("cx")) - parseFloat(player.attr("cx"));
+    var yDiff = parseFloat(enemy.attr("cy")) - parseFloat(player.attr("cy"));
+
+    var separation = Math.sqrt( Math.pow(xDiff,2) + Math.pow(yDiff,2) );
+    if (separation < radiusSum) {
+      collidedCallback();
+    }
+  });
+};
+
+var onCollision = function () {
+  // d3.select("svg")
+  svg.transition()
+    .attr("class", "hit");
+  setTimeout(function(){
+    svg.transition()
+    .attr("class","");
+  },90);
+};
 
 var deathBalls = [1,2,3,4,5,6,7,8,9,10];
 
 create(deathBalls);
 
 setInterval(function() {
-  update();
+  updateEnemies();
 }, 1000);
+
+setInterval(function() {
+  checkCollision(onCollision);
+}, 30);
